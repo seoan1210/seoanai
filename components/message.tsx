b5fa56b5e5446f4e639532be38b4bbda6218f882
seoan1210 <seoan102412@gmail.com>
2025-08-08 15:56:42 +0900

    Update message.tsx

--- components/message.tsx
+++ components/message.tsx
@@ -364,7 +364,7 @@ export const ThinkingMessage = () => {
 
         <div className="flex flex-col gap-2 w-full">
           <div className="flex flex-col gap-4 text-muted-foreground">
-            Hmm...
+            잠시만 기다려 주세요...
           </div>
         </div>
       </div>
