d90c1b4025b73aec2cfb1b14ec4be0eb6c7c5d3a
seoan1210 <seoan102412@gmail.com>
2025-08-22 19:06:43 +0900

    Create page.tsx

---
+++ app/(chat)/page.tsx
@@ -0,0 +1,28 @@
+import { cookies } from 'next/headers';
+import { DEFAULT_CHAT_MODEL } from '@/lib/ai/models';
+import { generateUUID } from '@/lib/utils';
+import { auth } from '../(auth)/auth';
+import { redirect } from 'next/navigation';
+import ClientPage from './client-page';
+
+export default async function Page() {
+  const session = await auth();
+
+  if (!session) {
+    redirect('/api/auth/guest');
+  }
+
+  const id = generateUUID();
+
+  const cookieStore = await cookies();
+  const modelIdFromCookie = cookieStore.get('chat-model');
+
+  return (
+    <ClientPage 
+      initialChatModel={DEFAULT_CHAT_MODEL}
+      session={session}
+      id={id}
+      modelIdFromCookie={modelIdFromCookie}
+    />
+  );
+}
