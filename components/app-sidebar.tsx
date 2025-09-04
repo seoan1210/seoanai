dcfa003d26e2538874eed1c433c01b3d5e547461
seoan1210 <seoan102412@gmail.com>
2025-09-04 22:19:56 +0900

    Update app-sidebar.tsx

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -53,7 +53,7 @@ export function AppSidebar({ user }: { user: User | undefined }) {
                   <PlusIcon />
                 </Button>
               </TooltipTrigger>
-              <TooltipContent align="end">New Chat</TooltipContent>
+              <TooltipContent align="end">새 채팅</TooltipContent>
             </Tooltip>
           </div>
         </SidebarMenu>
