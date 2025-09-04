9849280d45571033948f1f08d1431420788ec1de
seoan1210 <seoan102412@gmail.com>
2025-09-04 22:29:56 +0900

    Update greeting.tsx

--- components/greeting.tsx
+++ components/greeting.tsx
@@ -13,7 +13,7 @@ export const Greeting = () => {
         transition={{ delay: 0.5 }}
         className="text-2xl font-semibold"
       >
-        Hello there!
+        안녕하세요
       </motion.div>
       <motion.div
         initial={{ opacity: 0, y: 10 }}
@@ -22,7 +22,7 @@ export const Greeting = () => {
         transition={{ delay: 0.6 }}
         className="text-2xl text-zinc-500"
       >
-        How can I help you today?
+        무엇을 도와드릴까요?
       </motion.div>
     </div>
   );
