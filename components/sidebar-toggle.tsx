d5928cc3d06fa62e41c9d8d4117cef4e2fde4b9c
Seoan <seoan102412@gmail.com>
2025-09-11 22:12:05 +0900

    Update sidebar-toggle.tsx

--- components/sidebar-toggle.tsx
+++ components/sidebar-toggle.tsx
@@ -28,7 +28,7 @@ export function SidebarToggle({
         </Button>
       </TooltipTrigger>
       <TooltipContent align="start" className="hidden md:block">
-        Toggle Sidebar
+        메뉴
       </TooltipContent>
     </Tooltip>
   );
