04ce199b59a2c9df7becb71c3517bf43add6ee5b
Seoan <seoan102412@gmail.com>
2025-09-11 21:13:03 +0900

    Update page.tsx

--- app/(chat)/page.tsx
+++ app/(chat)/page.tsx
@@ -1,11 +1,13 @@
-import { cookies } from 'next/headers';
 
+
+import { cookies } from 'next/headers';
 import { Chat } from '@/components/chat';
 import { DEFAULT_CHAT_MODEL } from '@/lib/ai/models';
 import { generateUUID } from '@/lib/utils';
 import { DataStreamHandler } from '@/components/data-stream-handler';
 import { auth } from '../(auth)/auth';
 import { redirect } from 'next/navigation';
+import Link from 'next/link';
 
 export default async function Page() {
   const session = await auth();
